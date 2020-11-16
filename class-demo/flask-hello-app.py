8f01153a5435ac8333ff25139547550aa132d04b
Mohamed-98 <m.gado86@gmail.com>
2020-11-16 02:08:49 +0200

    done

--- class-demo/flask-hello-app.py
+++ class-demo/flask-hello-app.py
@@ -4,7 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/test1'
+
 db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+
 
 @app.route('/')
 

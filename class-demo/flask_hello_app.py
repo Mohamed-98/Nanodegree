8c9595d63c9f244ae6713e53e009b065f4efc67d
Mohamed-98 <m.gado86@gmail.com>
2020-11-19 14:03:48 +0200

    Working

--- class-demo/flask_hello_app.py
+++ class-demo/flask_hello_app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 
 
 class Person(db.Model):
-  __tablename__ = 'persons3'
+  __tablename__ = 'persons'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
 

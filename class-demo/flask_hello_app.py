2e5f6abc4bee59a4497b97b5cceb43be3a40bd2b
Mohamed-98 <m.gado86@gmail.com>
2020-11-21 01:55:53 +0200

    last working

--- class-demo/flask_hello_app.py
+++ class-demo/flask_hello_app.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test1'
-
-#app.run()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 
 class Person(db.Model):
@@ -16,14 +12,14 @@ class Person(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(), nullable=False)
 
-
-def __repr__(self):
+  def __repr__(self):
     return f'<Person ID: {self.id}, name: {self.name}>'
+
 
 db.create_all()
 
-@app.route('/')
 
+@app.route('/')
 def index():
   person = Person.query.first()
   return 'Hello ' + person.name
